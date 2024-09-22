ed0807e8fb8f923e82f327e019bbbea634716bd0
SayanSarkar7 <sarkarsayan602@gmail.com>
2024-09-22 12:45:20 +0530

    array sort performed

--- Basic/JS/array.js
+++ Basic/JS/array.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+
+// performing sort()
+let myArr=[2,77,4,33,0,9];
+//  myArr.sort();
+//  console.log(myArr); //  o/p:  [ 0, 2, 33, 4, 77, 9 ]
+ 
+myArr.sort((a,b)=>{
+    return a-b;
+})
+console.log(myArr); // o/p: [ 0, 2, 4, 9, 33, 77 ]
+
+
+
+
