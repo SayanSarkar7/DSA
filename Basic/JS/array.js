c80694332fc960a72a6bd221d968be06ee36c253
SayanSarkar7 <sarkarsayan602@gmail.com>
2024-09-22 13:09:09 +0530

    array reduce performed

--- Basic/JS/array.js
+++ Basic/JS/array.js
@@ -1,14 +1,19 @@
-
 // performing sort()
-let myArr=[2,77,4,33,0,9];
+let myArr = [2, 77, 4, 33, 9];
 //  myArr.sort();
 //  console.log(myArr); //  o/p:  [ 0, 2, 33, 4, 77, 9 ]
- 
-myArr.sort((a,b)=>{
-    return a-b;
-})
-console.log(myArr); // o/p: [ 0, 2, 4, 9, 33, 77 ]
 
+// myArr.sort((a,b)=>{
+//     return a-b;
+// })
+// console.log(myArr); // o/p: [ 0, 2, 4, 9, 33, 77 ]
 
+let max = Math.max(...myArr);
+let min = Math.min(...myArr);
+console.log(max, min); // o/p: 77 0
 
+let sumOfArr = myArr.reduce((a, b) => {
+  return a + b;
+},1);
+console.log( sumOfArr); //125+1(sum value initializes with value 1) = 126
 
