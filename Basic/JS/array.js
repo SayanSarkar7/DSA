d1266baa9c40f3209ce6457e2567fcec441bde2e
SayanSarkar7 <sarkarsayan602@gmail.com>
2024-09-22 13:33:39 +0530

    array reverse performed

--- Basic/JS/array.js
+++ Basic/JS/array.js
@@ -8,12 +8,16 @@ let myArr = [2, 77, 4, 33, 9];
 // })
 // console.log(myArr); // o/p: [ 0, 2, 4, 9, 33, 77 ]
 
-let max = Math.max(...myArr);
-let min = Math.min(...myArr);
-console.log(max, min); // o/p: 77 0
+// let max = Math.max(...myArr);
+// let min = Math.min(...myArr);
+// console.log(max, min); // o/p: 77 0
 
-let sumOfArr = myArr.reduce((a, b) => {
-  return a + b;
-},1);
-console.log( sumOfArr); //125+1(sum value initializes with value 1) = 126
+// let sumOfArr = myArr.reduce((a, b) => {
+//   return a + b;
+// },1);
+// console.log( sumOfArr); //125+1(sum value initializes with value 1) = 126
+
+let newArr=[...myArr].reverse();
+console.log(myArr,"<-old------new->",newArr);
+
 
