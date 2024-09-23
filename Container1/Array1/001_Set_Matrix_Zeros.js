7d08cc1013d4c9f95e976303439bb4a8eeb25e3e
SayanSarkar7 <sarkarsayan602@gmail.com>
2024-09-23 22:41:04 +0530

    solved array 1 with better approach

--- Container1/Array1/001_Set_Matrix_Zeros.js
+++ Container1/Array1/001_Set_Matrix_Zeros.js
@@ -3,9 +3,8 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 
-
-// Brute force Solution
-
+// Brute force Solution with T.C = O[n3]
+/*
 var setZeroes = function(matrix) {
     for(let i=0;i<matrix.length;i++){
      for(let j=0;j<matrix[i].length;j++){
@@ -38,7 +37,36 @@ var setZeroes = function(matrix) {
     }
     
  };
- 
- 
- 
- 
+ */
+
+//Better Approach
+/* We will take two one dimentional arrays representing rows and columns. If we find 0's in that matrix then the corresponding rowArray and columnArray will be marked.Finally we will check that if any one of rowArray and columnArray is marked then the element will be 0.  */
+
+var setZeroes = function (matrix) {
+  let rowArr = [];
+  let colArr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        rowArr.push(i);
+        colArr.push(j);
+      }
+    }
+  }
+
+  for (let j = 0; j < matrix[matrix[0]].length; j++) {
+    for (let r = 0; r < rowArr.length; r++) {
+      if (matrix[r][j] != 0) {
+        matrix[r][j] = 0;
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let c = 0; c < colArr.length; c++) {
+      if (matrix[i][c] != 0) {
+        matrix[i][c] = 0;
+      }
+    }
+  }
+};
